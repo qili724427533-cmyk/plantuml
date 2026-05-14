ff7a693e32e3328691772a72555933f81247164c
Arnaud Roques <plantuml@gmail.com>
2026-05-14 14:39:57 +0200

    🍵 add support of OpenIconic icons in TeaVM

--- src/main/java/net/sourceforge/plantuml/klimt/creole/atom/AtomOpenIconic.java
+++ src/main/java/net/sourceforge/plantuml/klimt/creole/atom/AtomOpenIconic.java
@@ -42,26 +42,26 @@ import net.sourceforge.plantuml.klimt.font.StringBounder;
 import net.sourceforge.plantuml.klimt.geom.XDimension2D;
 import net.sourceforge.plantuml.klimt.shape.TextBlock;
 import net.sourceforge.plantuml.klimt.shape.TextBlockUtils;
-import net.sourceforge.plantuml.openiconic.OpenIcon;
+import net.sourceforge.plantuml.openiconic.OpenIconic;
 import net.sourceforge.plantuml.url.Url;
 
-public class AtomOpenIcon extends AbstractAtom implements Atom {
+public class AtomOpenIconic extends AbstractAtom implements Atom {
 
-	private final OpenIcon openIcon;
+	private final OpenIconic openIconic;
 	private final double factor;
 	private final Url url;
 	private final HColor color;
 
-	public AtomOpenIcon(HColor newColor, double scale, OpenIcon openIcon, FontConfiguration fontConfiguration,
+	public AtomOpenIconic(HColor newColor, double scale, OpenIconic openIconic, FontConfiguration fontConfiguration,
 			Url url) {
 		this.url = url;
-		this.openIcon = openIcon;
+		this.openIconic = openIconic;
 		this.factor = scale * fontConfiguration.getSize2D() / 12.0;
 		this.color = newColor == null ? fontConfiguration.getColor() : newColor;
 	}
 
 	private TextBlock asTextBlock() {
-		return TextBlockUtils.withMargin(openIcon.asTextBlock(color, factor), 1, 0);
+		return TextBlockUtils.withMargin(openIconic.asTextBlock(color, factor), 1, 0);
 	}
 
 	@Override
